ada24ee4e8fe9f9ff77cf9aeaf2d807ffa681a1d
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-03-09 19:45:14 +0100

    Pruebas en el update

--- src/Componentes/Forms/pruebaUpload.jsx
+++ src/Componentes/Forms/pruebaUpload.jsx
@@ -1,35 +1,31 @@
-import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useLocalStorage } from "../../Hooks";
 
 const PruebaUpload = () => {
 
-    const [image, setImage] = useState({});
+    const [token] = useLocalStorage('token')
+
+    const [image, setImage] = useState({})
 
     const fileOnChange = (event) => {
         setImage(event.target.files[0])
     };
 
-    const sendImage = async (event) => {
+    const sendImage = (event) => {
         let formData = new FormData();
 
         formData.append('avatar', image);
 
-        // async function fetchData() {
-        //     const response = await
-        //         axios.get(`http://localhost:3000/users/groups/${groupId}`)
-        // }
-        // fetchData();
-
-        // fetch("http://localhost:3000/users/uploads/uploads", {
-        //     method: "post",
-        //     body: formData,
-        // }).then((res) => res.json()).catch((res) => res.json())
-
-
-        await axios.post("http://localhost:3000/users/uploads/uploads", image).then(() => {
-
-        }).catch((error) => { console.log(error) })
-    }
+        fetch('http://localhost:3000/users/uploads/uploads', {
+            mode: 'cors',
+            method: "post",
+            body: formData,
+        })
+            .then((res) => res.text())
+            .then((resBody) => {
+                console.log(resBody);
+            });
+    };
 
     return (
         <div>
@@ -37,6 +33,6 @@ const PruebaUpload = () => {
             <button onClick={sendImage}>Upload</button>
         </div>
     )
-}
 
+}
 export default PruebaUpload
